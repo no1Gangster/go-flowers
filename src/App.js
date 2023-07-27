7005c9fed1282ad813c36b2c6ffa01c8a4c1d60f
Adyasha Panda <adyashap800@gmail.com>
2023-07-27 09:46:45 +0530

    Product Page Css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
-import AllRoutes from './AllRoutes';
-import './App.css';
-import ProductPage from './productPage/productPage';
-
+import AllRoutes from "./AllRoutes";
+import "./App.css";
+import ProductPage from "./productPage/productPage";
+import {
+  Box,
+  Center,
+  Text,
+  Heading,
+  Centre,
+  Divider,
+  Flex,
+} from "@chakra-ui/react";
 
 function App() {
   return (
-    <AllRoutes />
+    <div>
+      <Box bg="purple" p="0.1%">
+        <Text textAlign="center" textColor="white">
+          PASSPORT MEMBER : 25% OFF + FREE SHIPPING | GET THE DEAL!
+        </Text>
+      </Box>
+
+      <Box p="1%">
+        <Flex>
+          <Box m="auto" w="40%">
+            <Heading fontWeight="extrabold" fontSize="medium" color="purple">
+              Summer Flower Arrangements & Gifts
+            </Heading>
+          </Box>
+          <Center height="100px">
+            <Divider orientation="vertical" color="red" />
+          </Center>
+          <Box ml="10%">
+            <Flex flexDirection="column">
+              <Text fontSize="sm" fontWeight="bold">
+                Summer of Smiles®
+              </Text>
+              <Text fontSize="x-small">
+                It’s the season to celebrate, and we have more ways than ever to
+                send a smile! From colorful summer flower arrangements, bouquets
+                & summer wreaths to blooming summer plants & fun summer gifts
+                for delivery, trust us to help you celebrate the season!
+              </Text>
+            </Flex>
+          </Box>
+        </Flex>
+      </Box>
+
+      <AllRoutes />
+    </div>
   );
 }
 
--- src/productPage/quickLinks.jsx
+++ src/productPage/quickLinks.jsx
@@ -2,23 +2,26 @@ import React from "react";
 import "./quickLinks.css";
 import { Link } from "react-router-dom";
 
-function QuickLinks () {
-    return(
-        <div>
-            <div id="linkbox1">
-                <h4>Popular Categeories</h4>
-                <Link to = {"/summer"}><a>Summer</a></Link>
-                <a href="#">Birthday</a>
-                <a href="#">Plants</a>
-            </div>
-            <div id="linkbox2">
-                <h4>Popular Categeories</h4>
-                <a href="#">Summer</a>
-                <a href="#">Birthday</a>
-                <a href="#">Plants</a>
-            </div>
-        </div>
-    );
+function QuickLinks() {
+  return (
+    <div id="left_box">
+      <div id="linkbox1">
+        <h4>POPULAR SUMMER FLOWERS</h4>
+        <Link to={"/summer"}>Summer</Link>
+        <Link>Daises</Link>
+        <Link>Roses</Link>
+        <Link>Lilies</Link>
+      </div>
+      <div id="linkbox2">
+        <h4>SUMMER OCCASIONS</h4>
+        <Link>Pride</Link>
+        <Link>Father's day</Link>
+        <Link>Fourth of July</Link>
+        <Link>Summer BBQs & Grilling</Link>
+        <Link>Summer Birthday</Link>
+      </div>
+    </div>
+  );
 }
 
 export default QuickLinks;
